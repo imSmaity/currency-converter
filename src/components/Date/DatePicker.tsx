8c1413f26970dbd71e4d2723a1af05f0397e3259
imSmaity <sumanmaity305@gmail.com>
2024-07-13 19:04:38 +0530

    input box

--- src/components/Date/DatePicker.tsx
+++ src/components/Date/DatePicker.tsx
@@ -4,7 +4,14 @@ import './styles.css'
 const DatePicker: React.FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
   const today = new Date().toISOString().split('T')[0]
 
-  return <input type="date" className={'container ' + props.className} max={today} {...props} />
+  return (
+    <input
+      type="date"
+      className={'container date-picker ' + props.className}
+      max={today}
+      {...props}
+    />
+  )
 }
 
 export default DatePicker
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,7 +2,7 @@ import React, { InputHTMLAttributes } from 'react'
 import './styles.css'
 
 const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
-  return <input className="container" {...props} />
+  return <input className="container input-2" {...props} />
 }
 
 export default Input
