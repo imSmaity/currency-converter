6bd27026e0d256fe3d236c677ddab323ce35bf51
imSmaity <sumanmaity305@gmail.com>
2024-07-13 19:28:16 +0530

    fix: date component

--- src/utils/getDate.tsx
+++ src/utils/getDate.tsx
@@ -2,7 +2,7 @@ const getDate = (inputDate?: string) => {
   const newDate = inputDate ? new Date(inputDate) : new Date()
 
   const day = newDate.getDate()
-  const month = newDate.getMonth()
+  const month = newDate.getMonth() + 1
   const year = newDate.getFullYear()
 
   const fullDate = `${year}-${month < 10 ? `0` : ''}${month}-${day < 10 ? `0` : ''}${day}`
